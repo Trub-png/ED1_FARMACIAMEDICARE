da7afa0c8c1452bf4f8bd0e02a59caf622f71820
Ian A <ianvegita831@gmail.com>
2026-07-23 08:29:53 -0600

    Conexion DataBase

---
+++ src/Conexion/ConexionDB.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.*;
+
+public class ConexionDB {
+    
+    // Manejo de las constantes de datos de la conexión
+    private static final String URL = "jdbc:mysql://localhost:3306/sistema_escolar";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    
+    // Método para realizar la conexión
+    public static Connection conexion(){
+        
+        Connection conn = null;
+        
+        // Manejar el error en la conexión con la base de datos
+        try{
+            
+            // Guardar conexión en el objeto Connection
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conexión exitosa");
+            
+            System.out.println("Cambio en el repositorio local");
+            
+            
+        }catch(SQLException e){
+            // Manejar el error de SQL y mostrarlo al usuario
+            System.out.println("Error en la conexion: " + e.getMessage());
+        }
+        
+        return conn;
+        
+    }
+    
+}
