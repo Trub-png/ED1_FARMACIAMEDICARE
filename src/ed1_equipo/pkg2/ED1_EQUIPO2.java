65d89b3bbbf2bc42b2def155762e7f9d6a89041a
Trub-png <rsbeine@gmail.com>
2026-07-23 07:49:10 -0600

    primer commit

---
+++ src/ed1_equipo/pkg2/ED1_EQUIPO2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ed1_equipo.pkg2;
+
+/**
+ *
+ * @author TrubV
+ */
+public class ED1_EQUIPO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
