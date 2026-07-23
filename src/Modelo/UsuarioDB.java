75733fabecabea1cd54c65f110268d33de662e76
Ian A <ianvegita831@gmail.com>
2026-07-23 11:36:56 -0600

    Actualizacion de los controllers

--- src/Modelo/UsuarioDB.java
+++ src/Modelo/UsuarioDB.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
 import java.sql.Connection;
@@ -12,19 +8,31 @@ import java.sql.SQLException;
 import Conexion.ConexionDB;
 
 public class UsuarioDB {
-    public Usuario validarLogin(String nombre, String password) {
-        String sql = "SELECT * FROM Usuario WHERE nombre = ? AND password = ?";
+    
+    public Usuario validarLogin(String usuario, String password) {
+        
+        String sql = "SELECT * FROM usuarios WHERE usuario = ? AND password = ?";
+        
         try (Connection conn = ConexionDB.conexion();
              PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, nombre);
+            
+            ps.setString(1, usuario);
             ps.setString(2, password);
+            
             ResultSet rs = ps.executeQuery();
+            
             if (rs.next()) {
-                return new Usuario(rs.getInt("id_usuario"), rs.getString("correo"), rs.getString("contrasena"), 
-                                   rs.getString("nombre"), rs.getString("telefono"), rs.getString("fecha_nac"));
+                return new Usuario(
+                    rs.getInt("id_usuario"),
+                    rs.getString("usuario"),
+                    rs.getString("password")
+                );
             }
-        } catch (SQLException e) { System.out.println("Error login: " + e.getMessage()); }
+            
+        } catch (SQLException e) {
+            System.out.println("Error login: " + e.getMessage());
+        }
+        
         return null;
     }
-
 }
